2a81796ca859df6ee317ac103a840ee31e0323ea
ChinmayKaitade <chinmaykaitade123@gmail.com>
2026-03-10 12:06:29 +0530

    10Section-JS Refresher Add On: Operations in JavaScript code files added

---
+++ 10Section-JS-Refresher-Add-On/01JS-Foundation/part1/operations.js
@@ -0,0 +1,27 @@
+let score = 102;
+let bonus = 25;
+
+let totalScore = score + bonus;
+
+let addition = 4 + 5;
+let subtract = 9 - 3;
+let multiplication = 3 * 5;
+let division = 8 / 2;
+let reminder = 9 % 2;
+let exponent = 2 ** 3;
+
+let myScore = 110;
+myScore++;
+
+let credits = 56;
+credits--;
+
+// Comparison operation
+let num1 = 3;
+let num2 = 3;
+let num3 = 6;
+
+console.log(num1 == num2); //true
+console.log(num1 != num3); // true
+console.log(num1 > num3); // true
+console.log(num1 < num3); // true
