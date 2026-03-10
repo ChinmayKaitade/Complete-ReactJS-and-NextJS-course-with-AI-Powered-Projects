6cebec7f8b5e494cb28ec6d81c92f59c9450a232
ChinmayKaitade <chinmaykaitade123@gmail.com>
2026-03-10 15:33:46 +0530

    10Section-JS Refresher Add On: Non Primitives in JavaScript code files added

---
+++ 10Section-JS-Refresher-Add-On/01JS-Foundation/part2/non-primitive.js
@@ -0,0 +1,24 @@
+const username = {
+  firstName: "chinmay",
+  isLoggedIn: true,
+};
+
+// username.firstName = "Mr. H";
+username.lastName = "kaitade";
+
+// console.log(username["firstName"]);
+// console.log(username.lastName);
+// console.log(username);
+// console.log(typeof username);
+
+let today = new Date();
+// console.log(today.getDate());
+
+// Array
+let anotherUser = ["chinmay", true];
+
+// console.log(anotherUser[0]);
+
+let isValue = "2abc";
+console.log(typeof Number(isValue));
+console.log(Number(null));
