4ab463c6fc4d5b9fe130dcdab57e5121a3656cfb
ChinmayKaitade <chinmaykaitade123@gmail.com>
2026-06-03 19:58:03 +0530

    12Section- Project Structure & File System Routing - Updated: Mini Project 1 - Making Responsive About Page code files added

--- 12Section-Project-Structure-&-File-System-Routing-Updated/Mini-Project-01/app/layout.js
+++ 12Section-Project-Structure-&-File-System-Routing-Updated/Mini-Project-01/app/layout.js
@@ -27,7 +27,7 @@ export default function RootLayout({ children }) {
     >
       <body className="min-h-full flex flex-col">
         <Navbar />
-        {children}
+        <main className="h-screen">{children}</main>
         <Footer />
       </body>
     </html>
